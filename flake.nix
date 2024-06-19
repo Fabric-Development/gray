cb5b84f14fddb3d89754d8be191e28834a575fc1
Muhammad Ahmad Chaudhary <61593188+muhchaudhary@users.noreply.github.com>
2024-06-19 10:03:35 -0400

    feat: add nix support (#1)

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "system trays for everyone ⚡";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    version = "0.1";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+
+    nativeBuildInputs = with pkgs; [
+      gobject-introspection
+      meson
+      pkg-config
+      ninja
+      vala
+    ];
+
+    buildInputs = with pkgs; [
+      glib
+      libdbusmenu-gtk3
+      gtk3
+    ];
+  in {
+    packages.${system} = rec {
+      default = gray;
+      gray = pkgs.stdenv.mkDerivation {
+        inherit nativeBuildInputs buildInputs;
+        pname = "libgray";
+        version = version;
+        src = ./.;
+        outputs = ["out" "dev"];
+      };
+    };
+
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs;
+      };
+    };
+  };
+}
